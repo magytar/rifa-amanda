e7b777537c69b59dbbcd184dc21bbb7263a3ab80
magytar <rafal2oko.3453@gmail.com>
2025-09-16 23:48:50 -0300

    atualizando2

--- src/app/page.js
+++ src/app/page.js
@@ -161,6 +161,7 @@ export default function RifaPage() {
                   maxLength="15"
                   className="w-full p-4 text-base border-2 font-black border-gray-200 rounded-2xl focus:border-pink-400 focus:outline-none transition-all duration-300 focus:shadow-lg focus:shadow-pink-100"
                   placeholder="(11) 99999-9999"
+                  style="-webkit-appearance: none;"
                 />
               </div>
 
